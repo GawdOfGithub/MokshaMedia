3c1676ba73c4ac65abc89e1bf4f5dfb7c923c2c2
GawdOfGithub <anuragbhandari77736@gmail.com>
2024-04-08 08:47:29 -0400

    Progress saved

--- app/end/page.tsx
+++ app/end/page.tsx
@@ -24,14 +24,17 @@ const Blog = () => {
     
    
     <div className="ml-[50px] mt-[40px] mb-[30px]">
+    <TabsTrigger value="account">
       <button className="shadow-[0_4px_14px_0_rgb(0,118,255,39%)] hover:shadow-[0_6px_20px_rgba(0,118,255,23%)] hover:bg-[rgba(0,118,255,0.9)] px-8 py-2 bg-[#0070f3] rounded-md text-white font-light transition duration-200 ease-linear mr-[10px]">
-      <TabsTrigger value="account">Call</TabsTrigger>
+     Call
       </button>
+      </TabsTrigger>
 
-   
+      <TabsTrigger value="Others">
       <button className="shadow-[0_4px_14px_0_rgb(0,118,255,39%)] hover:shadow-[0_6px_20px_rgba(0,118,255,23%)] hover:bg-[rgba(0,118,255,0.9)] px-8 py-2 bg-[#0070f3] rounded-md text-white font-light transition duration-200 ease-linear">
-      <TabsTrigger value="Others">Others</TabsTrigger>
+    Others
       </button>
+      </TabsTrigger>
     </div>
   </TabsList>
   <TabsContent value="account">  <Calendar
